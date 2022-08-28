1a34a276b6814e51f96078107b294ded3767bd8d
HenricoW <henrico.ashley.White@gmail.com>
2022-08-28 19:07:02 +0200

    feat: protected dashboard route, logout logic

--- src/components/Layout/Navbar.tsx
+++ src/components/Layout/Navbar.tsx
@@ -3,13 +3,17 @@ import React, { useState } from "react";
 import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 
-import { appName, navList } from "../../config";
+import { appName } from "../../config";
+import { logUserOut } from "../../core/user.service";
+import { useAppSelector } from "../../hooks/storeHooks";
 
 interface NavbarProps {
   toggleMobileMenu: () => void;
 }
 
 const Navbar = ({ toggleMobileMenu }: NavbarProps) => {
+  const userToken = useAppSelector((state) => state.user.token_id);
+
   return (
     <AppBar component="nav">
       <Toolbar>
@@ -26,11 +30,15 @@ const Navbar = ({ toggleMobileMenu }: NavbarProps) => {
           {appName}
         </Typography>
         <Box sx={{ display: { xs: "none", sm: "block" } }}>
-          {navList.map((item) => (
-            <Button key={item} sx={{ color: "#fff" }}>
-              {item}
-            </Button>
-          ))}
+          <Button>Home</Button>
+          {userToken && (
+            <>
+              <Button>Dashboard</Button>
+              <Button variant="contained" color="warning" onClick={logUserOut}>
+                Logout
+              </Button>
+            </>
+          )}
         </Box>
       </Toolbar>
     </AppBar>
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useAppSelector } from "../../hooks/storeHooks";
 
 const Dashboard = () => {
+  const userToken = useAppSelector((state) => state.user.token_id);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!userToken) navigate("/");
+  }, [userToken]);
+
   return <div>Dashboard</div>;
 };
 
